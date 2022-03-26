f5aa7f79d988d616b164a288261f420a0c7bd8d4
Daniel KALU <dankalu.work@gmail.com>
2022-03-26 18:28:51 +0000

    update db models and auth

---
+++ backend/models/charity.py
@@ -0,0 +1,21 @@
+from backend import db
+from datetime import datetime
+
+#charity user model 
+class Charity(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    charity_name = db.Column(db.String(50),nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100),nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    #about charity?? maybe to be added
+    #charity url???  maybe url
+    #one to many relationship with projects that the charity owns
+    projects_list = db.relationship('Project', backref='charity_projects', lazy=True)
+
+    #expand model to request charity registration documents at sign up (to vet authenticity of charity)
+
+    def __repr__(self):
+            return f'<Charity Name: {self.firstname} {self.lastname} Email: {self.email} Date Created: {self.date_created}>'
+
+
---
+++ backend/models/project.py
@@ -0,0 +1,21 @@
+from backend import db
+from datetime import datetime
+
+#charity project model
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), nullable=False) #project name
+    decsription = db.Column(db.String(600), nullable=False) #project description 
+    imageUrl=db.Column(db.String(600), nullable=False) # project image
+    goal=db.Column(db.String(600), nullable=False) # project goal
+    deadline=db.Column(db.DateTime,  nullable=False) #deadline to raise money
+    target_amount = db.Column(db.Integer(), nullable=False)  #amount to raise
+    raised_amount = db.Column(db.Integer(), nullable=False)  #currently raised
+    date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False) #porject creation date
+    charity_id = db.Column(db.Integer, db.ForeignKey('charity.id'), nullable=False)
+    donor_id = db.Column(db.Integer, db.ForeignKey('donor.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Project Title: {self.title} Target: {self.target_amount} Raised: {self.raised_amount} Date Created: {self.date_created}>'
+
+        
